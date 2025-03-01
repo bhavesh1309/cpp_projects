b52be7b7b1f25d84667107ffc0a371a0263be6c5
Bhavesh Kapil <bhaveshkapil09@gmail.com>
2025-03-01 16:43:47 +0530

    Added 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+// LOOP : FOR LOOP : infinite for loop
+for(int i=0;3<4;i++){
+    cout<<i<<endl;
+}
+
+
+//infinite while loop :
+int j=0;
+while(true)
+cout<<j<<endl;
+j++;
+
+//infinite do-while
+int k=0;
+do{
+    cout<<k<<endl;
+}
+while(true);
+return 0;
+}
